afc584b600956e4f64f4991f5de589b768c2f665
Dayana Barbosa <day_sbarbosa@outlook.com>
2026-07-13 15:39:03 -0300

    Configura rotas para cadastro e login

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Navbar } from './components/navbar/Navbar'
 import { Footer } from './components/footer/Footer'
 import Home from './pages/home/Home'
+import CadastrarUsuario from './pages/cadastrarUsuario/CadastrarUsuario'
+import LoginUsuario from './pages/loginUsuario/LoginUsuario'
 
 export default function App() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Navbar />
-      <main className="flex-grow">
-        <Home />
-      </main>
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cadastrar-usuario" element={<CadastrarUsuario />} />
+            <Route path="/login" element={<LoginUsuario />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
   )
 }
